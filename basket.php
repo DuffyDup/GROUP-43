f74559ad76044d65040bdf4e27f372aae17db7de
Mehakjot Singh <230201765@aston.ac.uk>
2024-12-06 22:20:22 +0000

    navigation add to basket page. my account page's css and php fixed. search bar added to he nav bar

--- basket.php
+++ basket.php
@@ -57,21 +57,12 @@ foreach ($basket_items as $item) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basket</title>
     <link rel="stylesheet" href="basket.css">
+    <link rel="stylesheet" href="main.css">
+    
 </head>
 <body>
-    <header>
-        <div class="logo">
-            <img src="Tech_Nova.png" alt="Logo">
-        </div>
-        <nav class="navigation">
-            <a href="">Home</a>
-            <a href="">About Us</a>
-            <a href="">Contact Us</a>
-            <a href="Login_Page.php">Login</a>
-            <a href="products.php">Products</a>
-            <a href="basket.php">Basket</a>
-        </nav>
-    </header>
+    <!-- Include the Navigation -->
+  <?php include 'Navbar.php'; ?>
 
     <div class="container">
         <h1>Your Basket</h1>
