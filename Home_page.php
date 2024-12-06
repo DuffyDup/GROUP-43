f66492175dab5c18197a87e86c3f2b299377fe58
Mehakjot Singh <230201765@aston.ac.uk>
2024-12-06 22:31:34 +0000

    naviton bar added to the home page

---
+++ Home_page.php
@@ -0,0 +1,16 @@
+<?php
+session_start(); // Start the session to check login status
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Signup Page</title>
+  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="Admin_signup_page.css">
+</head>
+<body>
+     <!-- Include the Navigation -->
+  <?php include 'Navbar.php'; ?>
+</body>
