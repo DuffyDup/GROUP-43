10c4d16534a1bd556fcb86e4e729351557a883bf
JHamzat <40691596+JHamzat@users.noreply.github.com>
2024-12-08 23:51:14 +0000

    Add appropriate title to page depending on product selected

--- productdetail.php
+++ productdetail.php
@@ -19,8 +19,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title><?= htmlspecialchars($product['name']) ?></title>
     <link rel="stylesheet" href="productdetail.css">
+    
 </head>
 <body>
     
