ebc7cf7cc4f1e87764a22f62f25c287bf1481386
Mehakjot Singh <230201765@aston.ac.uk>
2024-12-08 17:06:56 +0000

    linking nav bar in html

--- Contact_Us.php
+++ Contact_Us.php
@@ -7,6 +7,7 @@ session_start(); // Start the session to check login status
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="contactus.css">
 </head>
 <body>
