3de5a2f6e2aad368234d7c9221c48b85feaa4234
Shahin Cassime <230107249@aston.ac.uk>
2024-12-09 13:30:53 +0000

    changes

--- productdetail.php
+++ productdetail.php
@@ -1,7 +1,7 @@
 <?php
     session_start(); // Start the session to check login status
     require 'connectdb.php'; 
-    //$user_email = $_SESSION['email'];
+    $user_email = $_SESSION['email'];
  
     if (isset($_GET['product_id'])) {
     $product_id = $_GET['product_id'];
