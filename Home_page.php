6f7c624d2704c8f5df83c5aa780ace4f4a33e6d3
Mehakjot Singh <230201765@aston.ac.uk>
2025-02-05 09:30:01 +0000

    Changed the home page title from admin sign up to home page

--- Home_page.php
+++ Home_page.php
@@ -6,7 +6,7 @@ session_start();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin Signup Page</title>
+  <title>Home page</title>
   <link rel="stylesheet" href="main.css">
   <link rel="stylesheet" href="homepage.css">
 </head>
