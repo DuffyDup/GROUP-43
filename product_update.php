d6d6d79eb36d2f46dbe2583ed3e4daba3269be40
ManahilFirdous <230239322@aston.ac.uk>
2025-02-19 10:45:54 +0000

    Update product_update.php

--- product_update.php
+++ product_update.php
@@ -50,6 +50,8 @@
                     <th>Product Name</th>
                     <th>Product Description</th>
                     <th>Stock</th>
+                    <th></th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -59,8 +61,6 @@
                             <td><input type="text" name="product_name" required></td>
                             <td><input type="text" name="product_description" required></td>
                             <td><input type="number" name="stock" required></td>
-                            <td></td>
-                            <td></td>
                             <td>
                                 <button type="submit" name="add_product" class="btn update-btn">Add</button>
                                 <button type="submit" name="cancel_product" class="btn delete-btn" onclick="return confirm('Are you sure you want to cancel this update?');">Cancel</button>
