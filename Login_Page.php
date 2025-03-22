a16819bd5c0dd735e7b06bf7513c12302a91071f
ManahilFirdous <230239322@aston.ac.uk>
2025-03-22 07:08:56 +0000

    Update Login_Page.php

--- Login_Page.php
+++ Login_Page.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <?php include 'Navbar.php'; ?>
 
 <div class="loginuser">
-    <h1>Login</h1>
+    <h1 style="margin-bottom: 20px; font-size: 24px; color: #333;">Login</h1>
     <form id="loginforuser" method="POST" action="Login_Page.php">
         <input type="email" name="email" placeholder="Email" class="entrybox" required><br><br>
         <input type="password" name="password" placeholder="Password" class="entrybox" required><br><br>
